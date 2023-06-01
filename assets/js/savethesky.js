a3d4a387e31652f99933b2615561c7add4c0b7fb
RicardoSe14 <Ricardo.s14@gmail.com>
2023-06-01 08:11:55 -0400

    Revisión Final

--- assets/js/savethesky.js
+++ assets/js/savethesky.js
@@ -1,36 +1,19 @@
 
 
 
+var boton1 = $("#atiesto")[0];
+$("#tiesto")
+    .mouseenter(function() {
+        boton1.play()
+    });
 
-$("#fondo").click(function(){
-    $(".titulo1, .titulo2, #fondo").show(0);
-});
+    var boton1 = $("#atiesto")[0];
+$("#tiesto")
+    .mouseout(function() {
+        boton1.pause()
+    });
 
-$("#fondo").click(function(){
-    $('#galeria, #contacto, #ubicacion, .zoom, #fondo2').hide(0);
-});
-
-
-$("#minicio").click(function(){
-    $(".titulo1, .titulo2, #fondo").show(0);
-});
-
-$("#minicio").click(function(){
-    $('#galeria, #contacto, #ubicacion, .zoom, #fondo2').hide(0);
-});
-
-
-
-var boton2 = document.getElementById("guetta");
-boton2.addEventListener("mouseenter", function() { } );
-
-var v2 = document.getElementsByClassName("aguetta")[0];
-
-var sound2 = false;
-
-
-
- var boton2 = $("#aguetta")[0];
+var boton2 = $("#aguetta")[0];
  $("#guetta")
      .mouseenter(function() {
          boton2.play()
@@ -54,7 +37,164 @@ $("#oxia")
 		boton3.pause()
 	});
 
+    var boton4 = $("#aarmin")[0];
+    $("#armin")
+        .mouseenter(function() {
+            boton4.play()
+        });
+    
+        var boton4 = $("#aarmin")[0];
+        $("#armin")
+        .mouseout(function() {
+            boton4.pause()
+        });
 
+        var boton5 = $("#askrillex")[0];
+    $("#skrillex")
+        .mouseenter(function() {
+            boton5.play()
+        });
+    
+        var boton5 = $("#askrillex")[0];
+        $("#skrillex")
+        .mouseout(function() {
+            boton5.pause()
+        });
+
+        var boton6 = $("#aafrojack")[0];
+    $("#afrojack")
+        .mouseenter(function() {
+            boton6.play()
+        });
+    
+        var boton6 = $("#aafrojack")[0];
+        $("#afrojack")
+        .mouseout(function() {
+            boton6.pause()
+        });
+
+        var boton7 = $("#asaoki")[0];
+        $("#saoki")
+            .mouseenter(function() {
+                boton7.play()
+            });
+        
+            var boton7 = $("#asaoki")[0];
+            $("#saoki")
+            .mouseout(function() {
+                boton7.pause()
+            });
+
+
+            var boton8 = $("#almike")[0];
+            $("#lmike")
+                .mouseenter(function() {
+                    boton8.play()
+                });
+            
+                var boton8 = $("#almike")[0];
+                $("#lmike")
+                .mouseout(function() {
+                    boton8.pause()
+                });
+
+            var boton9 = $("#acdwitte")[0];
+            $("#cdwitte")
+                .mouseenter(function() {
+                    boton9.play()
+                });
+            
+            var boton9 = $("#acdwitte")[0];
+            $("#cdwitte")
+                .mouseout(function() {
+                    boton9.pause()
+                });
+
+            var boton10 = $("#addiablo")[0];
+            $("#ddiablo")
+                .mouseenter(function() {
+                    boton10.play()
+                });
+            
+            var boton10 = $("#addiablo")[0];
+            $("#ddiablo")
+                .mouseout(function() {
+                    boton10.pause()
+                });  
+    
+            var boton11 = $("#aabeyer")[0];
+            $("#abeyer")
+                .mouseenter(function() {
+                    boton11.play()
+                });
+            
+            var boton11 = $("#aabeyer")[0];
+            $("#abeyer")
+                .mouseout(function() {
+                    boton11.pause()
+                }); 
+
+            var boton12 = $("#amarshmello")[0];
+            $("#marshmello")
+                .mouseenter(function() {
+                    boton12.play()
+                });
+            
+            var boton12 = $("#amarshmello")[0];
+            $("#marshmello")
+                .mouseout(function() {
+                    boton12.pause()
+                }); 
+
+            var boton13 = $("#apickles")[0];
+            $("#pickles")
+                .mouseenter(function() {
+                    boton13.play()
+                });
+            
+            var boton13 = $("#apickles")[0];
+            $("#pickles")
+                .mouseout(function() {
+                    boton13.pause()
+                }); 
+
+            var boton14 = $("#aastrix")[0];
+            $("#astrix")
+                .mouseenter(function() {
+                    boton14.play()
+                });
+            
+            var boton14 = $("#aastrix")[0];
+            $("#astrix")
+                .mouseout(function() {
+                    boton14.pause()
+                }); 
+
+        var boton15 = $("#advegas")[0];
+        $("#dvegas")
+            .mouseenter(function() {
+                boton15.play()
+            });
+        
+            var boton15 = $("#advegas")[0];
+            $("#dvegas")
+            .mouseout(function() {
+                boton15.pause()
+            });
+
+            var boton16 = $("#afisher")[0];
+            $("#fisher")
+            .mouseenter(function() {
+                boton16.play()
+            });
+        
+            var boton16 = $("#afisher")[0];
+            $("#fisher")
+            .mouseout(function() {
+                boton16.pause()
+            });
+    
+        
 
 
 
