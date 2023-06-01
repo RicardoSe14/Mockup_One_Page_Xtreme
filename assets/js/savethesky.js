c8a895e7568903ab6d3eddbe096641853aa4b179
RicardoSe14 <Ricardo.s14@gmail.com>
2023-06-01 05:53:32 -0400

    Revisión Final

---
+++ assets/js/savethesky.js
@@ -0,0 +1,63 @@
+
+
+
+
+$("#fondo").click(function(){
+    $(".titulo1, .titulo2, #fondo").show(0);
+});
+
+$("#fondo").click(function(){
+    $('#galeria, #contacto, #ubicacion, .zoom, #fondo2').hide(0);
+});
+
+
+$("#minicio").click(function(){
+    $(".titulo1, .titulo2, #fondo").show(0);
+});
+
+$("#minicio").click(function(){
+    $('#galeria, #contacto, #ubicacion, .zoom, #fondo2').hide(0);
+});
+
+
+
+var boton2 = document.getElementById("guetta");
+boton2.addEventListener("mouseenter", function() { } );
+
+var v2 = document.getElementsByClassName("aguetta")[0];
+
+var sound2 = false;
+
+
+
+ var boton2 = $("#aguetta")[0];
+ $("#guetta")
+     .mouseenter(function() {
+         boton2.play()
+     });
+ 
+     var boton2 = $("#aguetta")[0];
+ $("#guetta")
+     .mouseout(function() {
+         boton2.pause()
+     });
+
+var boton3 = $("#aoxia")[0];
+$("#oxia")
+	.mouseenter(function() {
+		boton3.play()
+	});
+
+    var boton3 = $("#aoxia")[0];
+$("#oxia")
+	.mouseout(function() {
+		boton3.pause()
+	});
+
+
+
+
+
+
+
+
